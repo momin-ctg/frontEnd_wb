a4f65110bd8ed7e87c9017ec92c4a7dd6947d324
momin-ctg <mominriyadh@gmail.com>
2017-08-14 10:10:13 +0600

    added angularJS

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,286 @@
 var pmsFile = {
     "records": [
 
+        // page 20
+        {
+            "code": "Yellow 2X",
+            "hcolor": "#fce216"
+        },
+        {
+            "code": "116 2X",
+            "hcolor": "#f7b50c"
+        },
+        {
+            "code": "130 2X",
+            "hcolor": "#e29100"
+        },
+        {
+            "code": "165 2X",
+            "hcolor": "#ea4f00"
+        },
+        {
+            "code": "Warm Red",
+            "hcolor": "#e03a00"
+        },
+        {
+            "code": "1788 2x",
+            "hcolor": "#d62100"
+        },
+        {
+            "code": "1852 2x",
+            "hcolor": "#d11600"
+        },
+        {
+            "code": "485 2x",
+            "hcolor": "#cc0c00"
+        },
+        {
+            "code": "Rubine Red",
+            "hcolor": "#c6003d"
+        },
+        {
+            "code": "Rhodamine Red 2X",
+            "hcolor": "#d10572"
+        },
+        {
+            "code": "239 2X",
+            "hcolor": "#c4057c"
+        },
+        {
+            "code": "Purple",
+            "hcolor": "#aa0096"
+        },
+        {
+            "code": "2592 2X",
+            "hcolor": "#720082"
+        },
+        {
+            "code": "Violet 2X",
+            "hcolor": "#59008e"
+        },
+        {
+            "code": "Reflex Blue 2X",
+            "hcolor": "#1c007a"
+        },
+        {
+            "code": "Process Blue 2X",
+            "hcolor": "#0077bf"
+        },
+        {
+            "code": "299 2X",
+            "hcolor": "#007fcc"
+        },
+        {
+            "code": "306 2X",
+            "hcolor": "#00a3d1"
+        },
+        {
+            "code": "320 2X",
+            "hcolor": "#007f82"
+        },
+        {
+            "code": "327 2X",
+            "hcolor": "#008977"
+        },
+        {
+            "code": "Green 2X",
+            "hcolor": "#009677"
+        },
+        {
+            "code": "354 2X",
+            "hcolor": "#009944"
+        },
+        {
+            "code": "368 2X",
+            "hcolor": "#009e0f"
+        },
+        {
+            "code": "375 2X",
+            "hcolor": "#54bc00"
+        },
+        {
+            "code": "382 2X",
+            "hcolor": "#9ec400"
+        },
+        {
+            "code": "471 2X",
+            "hcolor": "#a34402"
+        },
+        {
+            "code": "464 2X",
+            "hcolor": "#704214"
+        },
+        {
+            "code": "433 2X",
+            "hcolor": "#0a0c11"
+        },
+        {
+            "code": "Black 2",
+            "hcolor": "#3a3321"
+        },
+        {
+            "code": "Black 3",
+            "hcolor": "#282d26"
+        },
+        {
+            "code": "Black 4",
+            "hcolor": "#3d3023"
+        }, {
+            "code": "Black 5",
+            "hcolor": "#422d2d"
+        },
+        {
+            "code": "Black 6",
+            "hcolor": "#1c2630"
+        },
+        {
+            "code": "Black 7",
+            "hcolor": "#443d38"
+        },
+        {
+            "code": "Black 2 2X",
+            "hcolor": "#111111"
+        },
+        {
+            "code": "Black 3 2X",
+            "hcolor": "#111114"
+        },
+        {
+            "code": "Black 4 2X",
+            "hcolor": "#0f0f0f"
+        },
+        {
+            "code": "Black 5 2X",
+            "hcolor": "#110c0f"
+        },
+        {
+            "code": "Black 6 2X",
+            "hcolor": "#070c0f"
+        },
+        {
+            "code": "Black 7 2X",
+            "hcolor": "#33302b"
+        },
+        {
+            "code": "801",
+            "hcolor": "#00aacc"
+        },
+        {
+            "code": "802",
+            "hcolor": "#33302b"
+        },
+        {
+            "code": "803",
+            "hcolor": "#ffed38"
+        },
+        {
+            "code": "804",
+            "hcolor": "#ff9338"
+        },
+        {
+            "code": "805",
+            "hcolor": "#f95951"
+        },
+        {
+            "code": "806",
+            "hcolor": "#ff0093"
+        },
+        {
+            "code": "807",
+            "hcolor": "#d6009e"
+        },
+        {
+            "code": "801 2X",
+            "hcolor": "#0089af"
+        },
+        {
+            "code": "802 2X",
+            "hcolor": "#0089af"
+        },
+
+        // 21 Page
+        {
+            "code": "803 2X",
+            "hcolor": "#ffd816"
+        },
+        {
+            "code": "804 2X",
+            "hcolor": "#ff7f1e"
+        },
+        {
+            "code": "805 2X",
+            "hcolor": "#f93a2b"
+        },
+        {
+            "code": "806 2X",
+            "hcolor": "#f7027c"
+        },
+        {
+            "code": "807 2X",
+            "hcolor": "#bf008c"
+        },
+        {
+            "code": "808",
+            "hcolor": "#bf008c"
+        },
+        {
+            "code": "809",
+            "hcolor": "#dde00f"
+        },
+        {
+            "code": "810",
+            "hcolor": "#ffcc1e"
+        },
+        {
+            "code": "811",
+            "hcolor": "#ff7247"
+        },
+        {
+            "code": "812",
+            "hcolor": "#fc2366"
+        },
+        {
+            "code": "813",
+            "hcolor": "#e50099"
+        },
+        {
+            "code": "814",
+            "hcolor": "#8c60c1"
+        },
+        {
+            "code": "808 2X",
+            "hcolor": "#00a087"
+        },
+        {
+            "code": "809 2X",
+            "hcolor": "#d6d60c"
+        },
+        {
+            "code": "810 2X",
+            "hcolor": "#ffbc21"
+        },
+        {
+            "code": "810 2X",
+            "hcolor": "#fc074f"
+        },
+        {
+            "code": "811 2X",
+            "hcolor": "#ff5416"
+        },
+        {
+            "code": "812 2X",
+            "hcolor": "#fc074f"
+        },
+        {
+            "code": "813 2X",
+            "hcolor": "#d10084"
+        },
+        {
+            "code": "814 2X",
+            "hcolor": "#703faf"
+        },
+
+        // 1st page
         {
             "code": "Process Yellow",
             "hcolor": "#f7e214"
@@ -111,7 +391,8 @@ var pmsFile = {
         {
             "code": "1215",
             "hcolor": "#f9e08c"
-        },{
+        },
+        {
             "code": "1225",
             "hcolor": "#ffcc49"
         },
@@ -158,6 +439,388 @@ var pmsFile = {
         {
             "code": "133",
             "hcolor": "#705b0a"
+        },
+
+        // 2nd page
+
+        {
+            "code": "134",
+            "hcolor": "#FDD26E"
+        },
+        {
+            "code": "135",
+            "hcolor": "#FDD26E"
+        },
+        {
+            "code": "136",
+            "hcolor": "#d88c02"
+        },
+        {
+            "code": "137",
+            "hcolor": "#fca311"
+        },
+        {
+            "code": "139",
+            "hcolor": "#af7505"
+        },
+        {
+            "code": "140",
+            "hcolor": "#7a5b11"
+        },
+        {
+            "code": "1345",
+            "hcolor": "#ffd691"
+        },
+        {
+            "code": "1355",
+            "hcolor": "#fcce87"
+        },
+        {
+            "code": "1365",
+            "hcolor": "#fcba5e"
+        },
+        {
+            "code": "1375",
+            "hcolor": "#f99b0c"
+        },
+        {
+            "code": "1385",
+            "hcolor": "#cc7a02"
+        },
+        {
+            "code": "1395",
+            "hcolor": "#996007"
+        },
+        {
+            "code": "1405",
+            "hcolor": "#6b4714"
+        },
+        {
+            "code": "141",
+            "hcolor": "#f2ce68"
+        },
+        {
+            "code": "142",
+            "hcolor": "#f2bf49"
+        },
+        {
+            "code": "144",
+            "hcolor": "#efb22d"
+        },
+        {
+            "code": "145",
+            "hcolor": "#c67f07"
+        },
+        {
+            "code": "146",
+            "hcolor": "#9e6b05"
+        },
+        {
+            "code": "147",
+            "hcolor": "#725e26"
+        },
+        {
+            "code": "148",
+            "hcolor": "#ffd69b"
+        },
+        {
+            "code": "149",
+            "hcolor": "#fccc93"
+        },
+        {
+            "code": "150",
+            "hcolor": "#fcad56"
+        },
+        {
+            "code": "151",
+            "hcolor": "#f77f00"
+        },
+        {
+            "code": "152",
+            "hcolor": "#dd7500"
+        },
+        {
+            "code": "153",
+            "hcolor": "#bc6d0a"
+        },
+        {
+            "code": "154",
+            "hcolor": "#995905"
+        },
+        {
+            "code": "1485",
+            "hcolor": "#ffb777"
+        },
+        {
+            "code": "1495",
+            "hcolor": "#ff993f"
+        },
+        {
+            "code": "1505",
+            "hcolor": "#f47c00"
+        },
+        {
+            "code": "Orange 021",
+            "hcolor": "#ef6b00"
+        },
+        {
+            "code": "1525",
+            "hcolor": "#b55400"
+        },
+        {
+            "code": "1535",
+            "hcolor": "#8c4400"
+        },
+        {
+            "code": "1545",
+            "hcolor": "#4c280f"
+        },
+        {
+            "code": "155",
+            "hcolor": "#f4dbaa"
+        },
+        {
+            "code": "156",
+            "hcolor": "#f2c68c"
+        },
+        {
+            "code": "157",
+            "hcolor": "#eda04f"
+        },
+        {
+            "code": "158",
+            "hcolor": "#e87511"
+        },
+        {
+            "code": "159",
+            "hcolor": "#c66005"
+        },
+        {
+            "code": "160",
+            "hcolor": "#9e540a"
+        },
+        {
+            "code": "161",
+            "hcolor": "#633a11"
+        },
+        {
+            "code": "1555",
+            "hcolor": "#f9bf9e"
+        },
+        {
+            "code": "1565",
+            "hcolor": "#fca577"
+        },
+        {
+            "code": "1575",
+            "hcolor": "#fc8744"
+        },
+        {
+            "code": "1585",
+            "hcolor": "#f96b07"
+        },
+        {
+            "code": "1595",
+            "hcolor": "#d15b05"
+        },
+        {
+            "code": "1605",
+            "hcolor": "#a04f11"
+        },
+        {
+            "code": "1615",
+            "hcolor": "#843f0f"
+        },
+
+        // Page 3
+
+        {
+            "code": "162",
+            "hcolor": "#f9c6aa"
+        },
+        {
+            "code": "163",
+            "hcolor": "#fc9e70"
+        },
+        {
+            "code": "164",
+            "hcolor": "#fc7f3f"
+        },
+        {
+            "code": "165",
+            "hcolor": "#f96302"
+        },
+        {
+            "code": "166",
+            "hcolor": "#dd5900"
+        },
+        {
+            "code": "167",
+            "hcolor": "#bc4f07"
+        },
+        {
+            "code": "1625",
+            "hcolor": "#f9a58c"
+        },
+        {
+            "code": "1635",
+            "hcolor": "#f98e6d"
+        },
+        {
+            "code": "1645",
+            "hcolor": "#f97242"
+        },
+        {
+            "code": "1655",
+            "hcolor": "#f95602"
+        },
+        {
+            "code": "1665",
+            "hcolor": "#f95602"
+        },
+        {
+            "code": "1675",
+            "hcolor": "#a53f0f"
+        },
+        {
+            "code": "169",
+            "hcolor": "#f9baaa"
+        },
+        {
+            "code": "170",
+            "hcolor": "#f98972"
+        },
+        {
+            "code": "171",
+            "hcolor": "#f9603a"
+        },
+        {
+            "code": "172",
+            "hcolor": "#f9603a"
+        },
+        {
+            "code": "173",
+            "hcolor": "#d14414"
+        },
+        {
+            "code": "174",
+            "hcolor": "#933311"
+        },
+        {
+            "code": "175",
+            "hcolor": "#6d3321"
+        },
+        {
+            "code": "176",
+            "hcolor": "#f9afad"
+        },
+        {
+            "code": "177",
+            "hcolor": "#f9827f"
+        },
+        {
+            "code": "178",
+            "hcolor": "#f9827f"
+        },
+        {
+            "code": "Warm Red",
+            "hcolor": "#f9827f"
+        },
+        {
+            "code": "179",
+            "hcolor": "#e23d28"
+        },
+        {
+            "code": "180",
+            "hcolor": "#c13828"
+        },
+        {
+            "code": "181",
+            "hcolor": "#7c2d23"
+        },
+        {
+            "code": "1765",
+            "hcolor": "#f99ea3"
+        },
+        {
+            "code": "1775",
+            "hcolor": "#f9848e"
+        },
+        {
+            "code": "1785",
+            "hcolor": "#fc4f59"
+        },
+        {
+            "code": "1788",
+            "hcolor": "#ef2b2d"
+        },
+        {
+            "code": "1795",
+            "hcolor": "#d62828"
+        },
+        {
+            "code": "1805",
+            "hcolor": "#af2626"
+        },
+        {
+            "code": "1815",
+            "hcolor": "#7c211e"
+        },
+        {
+            "code": "1767",
+            "hcolor": "#f9b2b7"
+        },
+        {
+            "code": "1777",
+            "hcolor": "#fc6675"
+        },
+        {
+            "code": "1787",
+            "hcolor": "#f43f4f"
+        },
+        {
+            "code": "Red 032",
+            "hcolor": "#ef2b2d"
+        },
+        {
+            "code": "1797",
+            "hcolor": "#ef2b2d"
+        },
+        {
+            "code": "1807",
+            "hcolor": "#a03033"
+        },
+        {
+            "code": "1817",
+            "hcolor": "#5b2d28"
+        },
+        {
+            "code": "182",
+            "hcolor": "#f9bfc1"
+        },
+        {
+            "code": "183",
+            "hcolor": "#fc8c99"
+        },
+        {
+            "code": "184",
+            "hcolor": "#fc5e72"
+        },
+        {
+            "code": "185",
+            "hcolor": "#e8112d"
+        },
+        {
+            "code": "186",
+            "hcolor": "#ce1126"
+        },
+        {
+            "code": "187",
+            "hcolor": "#af1e2d"
+        },
+        {
+            "code": "188",
+            "hcolor": "#7c2128"
         }
 
 
